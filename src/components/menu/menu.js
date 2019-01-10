0b7169b10fae7e66488bb79e1b2cffd398bbe6e7
Matheus Kruger <ms-kruger@hotmail.com>
2019-01-09 22:43:20 -0200

    responsivo

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -5,8 +5,12 @@ import './menu.scss';
 import { NavLink } from 'react-router-dom';
 
 class Menu extends Component {
-  toggleMenuMobile = () => {
-    document.querySelector('#main-menu').classList.toggle('open')
+  openMenuMobile = () => {
+    document.querySelector('#main-menu ul').classList.add('open')
+  }
+
+  closeMenuMobile = () => {
+    document.querySelector('#main-menu ul').classList.remove('open')
   }
 
   render() {
@@ -14,7 +18,9 @@ class Menu extends Component {
       <nav id="main-menu" className="reset section">
         <div className="content">
           <div className="col-12">
+            <button className="menu-mobile" onClick={this.openMenuMobile} >Menu</button>
             <ul>
+              <button className="close-mobile" onClick={this.closeMenuMobile}>Fechar</button>
               <li><NavLink to="/">Home</NavLink></li>
               <li><NavLink to="contato">Contato</NavLink></li>
               <li><NavLink to="sobre">Sobre</NavLink></li>
